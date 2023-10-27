2ea6de51ff7c75e746b6fa4fe4d4bcb74b0d3fc2
PedroRocha2 <pedro.rocha.neni@gmail.com>
2023-10-27 14:20:15 -0300

    feature :sparkles: busca até 60 sites com funcao scroll

--- api-server.js
+++ api-server.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()); // Middleware de parser de corpo JSON
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Permitir todas as origens
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Métodos permitidos
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Cabeçalhos permitidos
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
@@ -24,29 +24,46 @@ app.put("/scrape", async (req, res) => {
 
   try {
     await page.goto(linkParaOScrappe);
-    await page.waitForSelector("div .MjjYud");
 
+    // Rolar para baixo para carregar mais resultados
+    await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+        let totalHeight = 0;
+        const distance = 100;
+        const maxScrolls = 10; // Ajuste esse valor com base no número de rolagens necessárias
+        const timer = setInterval(() => {
+          const scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight || maxScrolls <= 0) {
+            clearInterval(timer);
+            resolve();
+          }
+          maxScrolls--;
+        }, 200); // Ajuste o intervalo se necessário
+      });
+    });
+
+    // Aguarde o seletor que contém os resultados da pesquisa
+    await page.waitForSelector("div .yuRUbf");
+
+    // Restante do  código para raspar os resultados
     const titulos = await page.evaluate(() => {
       const empresas = Array.from(document.querySelectorAll("div .yuRUbf h3"));
       return empresas.map((el) => el.textContent);
     });
 
-    console.log(titulos);
-
     const linksEmpresas = await page.evaluate(() => {
       const links = Array.from(document.querySelectorAll("div .yuRUbf a"));
       return links.map((el) => el.getAttribute("href"));
     });
-    console.log(linksEmpresas);
 
     const setores = await page.evaluate(() => {
       const setores = Array.from(document.querySelectorAll("div .VwiC3b span"));
       return setores.map((el) => el.textContent);
     });
 
-    console.log(setores);
-
-    // Criar objetos e adicionar ao array leads
     for (let i = 0; i < titulos.length; i++) {
       const lead = {
         titulo: titulos[i],
